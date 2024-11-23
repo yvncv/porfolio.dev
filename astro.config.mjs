4f40f406c668fe8fd0e1f6d3cfe07b72e3526266
yvncv <146147151+yvncv@users.noreply.github.com>
2024-11-23 06:23:27 -0500

    personalizacion

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,4 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
 })
